8c2d90cbbad69874f9e0c140fa4d99ad84c06b8d
bruno_hs_costa <bruno_hs_costa@estudante.sesisenai.org.br>
2023-03-22 12:14:07 -0300

    nb1

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nb1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-
+        nb1 = findViewById(R.id.Numb1);
     }
 }
