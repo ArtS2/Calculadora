1963033605cec09b8709ac61b270d71bce6c1c7c
arthur_marques-silva <marquesdasilvaarthur040@gmail.com>
2023-03-22 12:11:28 -0300

    Main java

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -6,21 +6,11 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        Numb1 = findViewById(R.id.Numb1);
-        Numb2 = findViewById(R.id.Numb2);
-        res = findViewById(R.id.resultado);
-    }
-    public void soma(View v) {
-        int Numb1 = Integer.parseInt(Numb1.getText().toString());
-        int Numb2 = Integer.parseInt(Numb2.getText().toString());
-        int resultadosoma = Numb1 + Numb2;
 
     }
-     res.setText(resultadosoma + " " );
 }
