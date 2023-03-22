fb5d8fd707310d3dfd6e2d348caf8b9d27f4d063
bruno_hs_costa <bruno_hs_costa@estudante.sesisenai.org.br>
2023-03-22 11:53:02 -0300

    java

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Numb1 = findViewById(R.id.Numb1);
+        Numb2 = findViewById(R.id.Numb2);
+        res = findViewById(R.id.resultado);
     }
+    public void soma(View v) {
+        int Numb1 = Integer.parseInt(Numb1.getText().toString());
+        int Numb2 = Integer.parseInt(Numb2.getText().toString());
+        int resultadosoma = Numb1 + Numb2;
+
+    }
+     res.setText(resultadosoma + " " );
 }
